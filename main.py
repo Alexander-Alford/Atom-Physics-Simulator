855b915de1cac8e905619ad5d3f37a11b3b40558
Alex <alexalford14@yahoo.com>
2021-02-01 01:55:56 -0800

    Simple Bohr model up

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from vpython import *
 import math
 
-#constants
-delta_t = 1e-13 # s
-min_dist = 1e-15 # m
+#mathematical constants
 elec_mass = 9.1093837015e-31 # kg 
 prot_mass = 1.6726219e-27 # kg
 elem_charge = 1.602176634e-19 # C
-scalefactor = 1e15
 coulombconst = 8.9875517923e9 # kg⋅m3⋅s^2/C^2
 
+#approx constants
+delta_t = 1e-27 # s
+min_dist = 1e-15 # m
+scalefactor = 1e15
 time = 0.0
 
 class Particle:
@@ -25,12 +26,7 @@ class Particle:
         self.position += (self.velocity*delta_t*scalefactor)
         self.sphere.pos = self.position
 
-    def applyForce(self, other):
-        force = getElectroForce(self.charge, other.charge, 
-                                getDistance(self.position, other.position))
-
-        self.velocity += (norm(self.position - other.position)*force*delta_t)/self.mass
-        other.velocity += (norm(other.position - self.position)*force*delta_t)/other.mass
+ 
 
 
 particlelist = []
@@ -46,29 +42,43 @@ def getElectroForce(q1, q2, rad):
 def addForce():
     return 
 
+def applyForce(p1, p2):
+    force = getElectroForce(p1.charge, p2.charge, 
+                            getDistance(p1.position, p2.position))
+
+    p1.velocity += (norm(p1.position - p2.position)*force*delta_t)/p1.mass
+    p2.velocity += (norm(p2.position - p1.position)*force*delta_t)/p2.mass
+    #print(p1.velocity)
+    #print(p2.velocity)
 
 
-primescene = canvas(width = 800, height = 800)
-
-
-particlelist.append(Particle([0,0,0], prot_mass, elem_charge, [-0.001,0,0], color.purple, 0.0005))
-particlelist.append(Particle([0,0.00001,0], elec_mass, -elem_charge, [0.001,0,0], color.yellow, 0.0005))
 
 
 
-while time < delta_t*10000:
-    rate(100)
+primescene = canvas(title="Atomic Physics Simulator", width = 800, height = 800, center=vector(0,0,0))
+
+
+button(text="Pause", pos=primescene.title_anchor, bind=addForce)
+
+
+particlelist.append(Particle([0,0,0], prot_mass, elem_charge, [0,0,0], color.purple, 1e-12))
+particlelist.append(Particle([0,7.18e-2,0], elec_mass, -elem_charge, [5.29e-11,0,0], color.yellow, 1e-12))
+
+
+
+while time < delta_t*90000:
+    rate(2000)
     for p in range(len(particlelist)):
         p1 = p
         p2 = p + 1
 
         try:
-            particlelist[p].applyForce(particlelist[p2])    
+            applyForce(particlelist[p1], particlelist[p2])    
         except:
-            i = 0 
+            pass
 
     particlelist[p].updatePos()         
-    print(particlelist[0].velocity)
+    #print(particlelist[0].velocity)
     time = time + delta_t
 
 del particlelist
