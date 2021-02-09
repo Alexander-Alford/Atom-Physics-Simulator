7b91e6edd0e6d5f439bc49cd1b084b7231229a5d
Alex <alexalford14@yahoo.com>
2021-02-09 01:39:23 -0800

    New graph for hydrogen s1 radial probability

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from vpython import *
 import math
 
+
 #mathematical constants
 elec_mass = 9.1093837015e-31 # kg 
 prot_mass = 1.6726219e-27 # kg
 elem_charge = 1.602176634e-19 # C
 coulombconst = 8.9875517923e9 # kg⋅m3⋅s^2/C^2
+a_0 = 5.29177210903e-11 # Bohr radius in m
+
 
 #approximation constants
 delta_t = 1e-27 # s
 min_dist = 1e-15 # m
 scalefactor = 1e15
 time = 0.0
+graph_radBin = a_0/5
+
+
 
 class Particle:
 
@@ -31,20 +37,18 @@ class Particle:
 
 particlelist = []
 
-def electroCheck(call):
+#flags and button functions
+run_flag = True
+electro_flag = True
+magnet_flag = False
+
+def electroCheck(self):
     global electro_flag
 
     if electro_flag == True:
         electro_flag = False
     else:
         electro_flag = True
-
-#calulators
-def getDistance(pos_1, pos_2):
-    return mag(pos_1 - pos_2)
-
-def getElectroForce(q1, q2, rad):
-    return q1*q2*coulombconst/(rad ** 2)
 
 def pauseBut(self):
     global run_flag
@@ -55,6 +59,26 @@ def pauseBut(self):
     else:
         run_flag = False
         self.text = "Run"
+
+#graphing
+distgraph = graph(width = 500, height = 300, xmin = 0, ymin = 0, xmax = 5, ymax = 1, title = "probability", xtitle = "Radius (a0)", ytitle = "relative probability", align = "left")
+
+hydOrbTheory = gcurve( color=color.blue )
+for r in range(0, 500, 1):
+    hydOrbTheory.plot( (r/100), (4)*((r/100) ** 2)*exp(-2*(r/100)) )
+
+
+print((4/a_0 ** 2)*((100/100) ** 2)*exp(-2*(100/100)/a_0))
+print(-2*(100/100)/a_0)
+
+#calulators
+def getDistance(pos_1, pos_2):
+    return mag(pos_1 - pos_2)
+
+def getElectroForce(q1, q2, rad):
+    return q1*q2*coulombconst/(rad ** 2)
+
+
 
 def addForce(force, p1, p2):
     p1.velocity += (norm(p1.position - p2.position)*force*delta_t)/p1.mass
@@ -67,16 +91,16 @@ def applyForce(plist):
                 addForce( getElectroForce(plist[p1].charge, plist[p2].charge, getDistance(plist[p1].position, plist[p2].position)), plist[p1], plist[p2])
 
 
-run_flag = True
-electro_flag = True
-
-primescene = canvas(title="Atomic Physics Simulator", width = 800, height = 800, center=vector(0,0,0))
 
 
+primescene = canvas(title="Atomic Physics Simulator", width = 800, height = 800, center=vector(0,0,0), align = "left")
+
+#Button code. Try and revise later.
 button(text = "Pause", pos = primescene.title_anchor, bind = pauseBut)
 checkbox(text = "Electrostatic Force", bind = electroCheck, checked = True, id = 0)
+checkbox(text = "Magnetic Force", checked = False, bind = electroCheck)
 
-sphere(pos=vector(1.29e-11,0,0), color=color.red, radius=1e-12)
+#sphere(pos=vector(1.29e-11,0,0), color=color.red, radius=1e-12)
 
 particlelist.append(Particle([0,0,0], prot_mass, elem_charge, [0,0,0], color.purple, 1e-12))
 particlelist.append(Particle([0,7.18e-2,0], elec_mass, -elem_charge, [5.29e-11,0,0], color.yellow, 1e-12))
